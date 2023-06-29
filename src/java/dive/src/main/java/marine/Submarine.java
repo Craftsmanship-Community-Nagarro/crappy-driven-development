af2f2ea50d91d49f608ce18b70d69fb5768215d5
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2023-06-29 16:30:18 +0200

    Replacing switch with if's

--- src/java/dive/src/main/java/marine/Submarine.java
+++ src/java/dive/src/main/java/marine/Submarine.java
@@ -6,21 +6,33 @@ import java.util.List;
 
 @Getter
 public class Submarine {
-    private Position position;
+  private Position position;
 
-    public Submarine(int horizontal, int depth) {
-        this.position = new Position(horizontal, depth);
+  public Submarine(int horizontal, int depth) {
+    this.position = new Position(horizontal, depth);
+  }
+
+  public void move(List<Instruction> instructions) {
+    instructions.forEach(this::move);
+  }
+
+  private void move(Instruction instruction) {
+
+
+    String text = instruction.getText();
+
+    if ("down".equals(text)) {
+      position = position.withDepth(position.getDepth() + instruction.getX());
+      return;
     }
 
-    public void move(List<Instruction> instructions) {
-        instructions.forEach(this::move);
+    if ("up".equals(text)) {
+      position = position.withDepth(position.getDepth() - instruction.getX());
+      return;
     }
 
-    private void move(Instruction instruction) {
-        position = switch (instruction.getText()) {
-            case "down" -> position.withDepth(position.getDepth() + instruction.getX());
-            case "up" -> position.withDepth(position.getDepth() - instruction.getX());
-            default -> position.withHorizontal(position.getHorizontal() + instruction.getX());
-        };
-    }
+    position = position.withHorizontal(position.getHorizontal() + instruction.getX());
+
+
+  }
 }
